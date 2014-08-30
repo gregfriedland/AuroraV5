d7aace53e083f89cef160f69d1968a48f1deb7ba
Greg Friedland <greg.friedland@gmail.com>
2014-08-29 20:19:59 -0700

    Refactor and fix support for leap

--- html/patterns.js
+++ html/patterns.js
@@ -1,4 +1,9 @@
-function Animator(numLeds, paletteMgr, updateLEDsFunc) {
+function mod(m, n) {
+  return ((m % n) + n) % n;
+}
+
+function Animator(numLeds, paletteMgr, delay, updateLEDsFunc,
+                  drawFunc, updateIndexFunc) {
   this.leds = [];
   for (var i=0; i<numLeds; i++) {
     this.leds.push([0,0,0]);
@@ -6,29 +11,39 @@ function Animator(numLeds, paletteMgr, updateLEDsFunc) {
   
   this.updateLEDsFunc = updateLEDsFunc;
   this.paletteMgr = paletteMgr;
+  this.delay = delay;
+  this.drawFunc = drawFunc;
+  this.updateIndexFunc = updateIndexFunc;
+  this.index = 0;
 }
 
-Animator.prototype.run = function(func, config, state) {
-  var newState = func(this.leds, this.updateLEDsFunc, this.paletteMgr.getCurrent(), config, state);
+Animator.prototype.run = function() {
+  this.index = this.updateIndexFunc(this.index);
+  this.drawFunc(this.leds, this.paletteMgr.getCurrent(), this.index);
+  this.updateLEDsFunc(this.leds);
   
   var anim = this;
-  setTimeout(function() { anim.run(func, config, newState); },
-             newState.delay);
+  setTimeout(function() { anim.run(); }, anim.delay);
 }
 
 
 
-function RotateGradient(leds, updateLEDsFunc, palette, config, state) {
+function drawGradient(leds, palette, startIndex) {
   for (var l=0; l<leds.length; l++) {
-    var rgb = palette.rgbs[Math.floor(((state.t + l) % leds.length) * palette.numColors / leds.length)];
+    var rgb = palette.rgbs[Math.floor(((startIndex + l) % leds.length) * palette.numColors / leds.length)];
     leds[l] = rgb;
   }
-
-  updateLEDsFunc(leds);
-  
-  state.t = (state.t + 1) % leds.length;
-  state.delay = 1000/getWaitTime(config.wait, config.waitRange[0], config.waitRange[1]);
-  return state;
 }
+
+
+// create a function that increments an index by a given amount each iteration
+function makeIndexIncrementFunc(incr, wrap) {
+  return function(index) {
+    return mod(index + incr, wrap);
+  }
+}
+
+
+  //state.delay = 1000/getWaitTime(config.wait, config.waitRange[0], config.waitRange[1]);
 
 
