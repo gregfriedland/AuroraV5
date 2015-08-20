5981145e949a1e8777199073c071fa78fb8a6ee7
Greg Friedland <greg.friedland@gmail.com>
2015-08-19 20:37:47 -0700

    fix pattern change timeout

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ var DEPTH = 48; // bit depth: 24 or 48
 var NUM_COLORS = 1<<12; // colors in the gradient of each palette
 var FPS = 30;
 var START_DRAWER = 'Bzr';
-var DRAWER_CHANGE_INTERVAL = 600000;
+var DRAWER_CHANGE_INTERVAL = 60000;
 var CAM_SIZE = (320, 240)
 var layoutLeftToRight = false; // only used for serial port connections
 
